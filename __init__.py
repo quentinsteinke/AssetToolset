e80c3ff1cbcbb44cbd546dcd56f105beb711906e
Quentin Steinke <quentinsteinke@gmail.com>
2022-07-28 21:46:34 -0400

    update

--- __init__.py
+++ __init__.py
@@ -10,7 +10,12 @@ bl_info = {
 def register():
     print("Enabled the addon")
 
+    scene = bpy.context.scene
+selected_objects = bpy.context.selectable_objects
+split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add
+
+for obj in selected_objects:
+    obj.split_custom_normals
+
 def unregister():
     print("Disabling the addon")
-
-scene = bpy.context.scene
