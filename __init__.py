783e337c822d0829ccb2e788000323dff9142604
Quentin Steinke <quentinsteinke@gmail.com>
2022-07-28 21:55:21 -0400

    now it works

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from posixpath import split
 import bpy
 import os
 
@@ -12,10 +13,11 @@ def register():
 
     scene = bpy.context.scene
 selected_objects = bpy.context.selectable_objects
-split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add
+split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add()
 
 for obj in selected_objects:
-    obj.split_custom_normals
+    print(obj.name)
+    split_custom_normals
 
 def unregister():
     print("Disabling the addon")
