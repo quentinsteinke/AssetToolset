09aed566c4a19d65eb25bb019b7c52a3d91ee192
Quentin Steinke <quentinsteinke@gmail.com>
2022-07-28 22:18:08 -0400

    still working

--- __init__.py
+++ __init__.py
@@ -12,11 +12,13 @@ def register():
     print("Enabled the addon")
 
     scene = bpy.context.scene
-selected_objects = bpy.context.selectable_objects
+selected_objects = bpy.context.selected_objects
 split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add()
+active_object = bpy.context.view_layer.objects.active
 
 for obj in selected_objects:
-    print(obj.name)
+    active_object = obj
+    print(active_object.name)
     split_custom_normals
 
 def unregister():
