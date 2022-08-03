089a348e4f3be4c16b2bf4cc5ef91b23b76c7b6f
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-03 16:34:22 -0400

    working

--- __init__.py
+++ __init__.py
@@ -10,15 +10,17 @@ bl_info = {
 def register():
     print("Enabled the addon")
 
-    scene = bpy.context.scene
+scene = bpy.context.scene
 selected_objects = bpy.context.selected_objects
 split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add()
 active_object = bpy.context.view_layer.objects.active
 
 for obj in selected_objects:
-    active_object = obj
-    print(active_object.name)
+    print(obj.name)
+    bpy.context.view_layer.objects.active = bpy.data.objects[obj.name]
     split_custom_normals
+    print("____________")
+    #split_custom_normals
 
 def unregister():
     print("Disabling the addon")
