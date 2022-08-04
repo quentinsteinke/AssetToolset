60272373446b85708a759022926aace8cece9aea
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-04 11:16:11 -0400

    fixed split normals and active object

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import time
 import os
 
 bl_info = {
-    "name": "Advanced Game Exporter",
+    "name": "Game Exporter",
     "blender": (3, 2, 1),
     "category": "Object",
 }
@@ -14,33 +14,23 @@ def register():
 #clearing Blenders console
 os.system('cls')
 
-#Defining Variables
-scene = bpy.context.scene
 selected_objects = bpy.context.selected_objects
-split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add()
-active_object = bpy.context.view_layer.objects.active
 
-#Looping through selected objects and adding custom split normals
-def split_normals():
-    global active_object
-    global split_custom_normals
+def add_split_normals():
     global selected_objects
 
-    print(active_object.name)
+    for obj in selected_objects:
+        bpy.context.view_layer.objects.active = bpy.context.scene.objects[obj.name]
+        bpy.ops.mesh.customdata_custom_splitnormals_add()
+
+def add_split_normals():
+    global selected_objects
 
     for obj in selected_objects:
-        print("____________")
-        print(obj.name)
-        print(active_object.name)
-        active_object = bpy.data.objects[obj.name]
-        #split_custom_normals
-        print(active_object.name)
-        print("____________")
-        time.sleep(1)
+        bpy.context.view_layer.objects.active = bpy.context.scene.objects[obj.name]
+        bpy.ops.mesh.customdata_custom_splitnormals_clear()
 
-    print(active_object.name)
-
-split_normals()
+add_split_normals()
 
 def unregister():
     print("Disabling the addon")
