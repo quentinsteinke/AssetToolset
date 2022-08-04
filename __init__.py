75b4e0ae57c241a54b196e1082c75c6b3155bb79
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-04 15:01:55 -0400

    got everything working as of now

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import collections
 import bpy
 import time
 import os
@@ -15,22 +16,53 @@ def register():
 os.system('cls')
 
 selected_objects = bpy.context.selected_objects
+scene_collection = bpy.context.scene.collection.children
 
 def add_split_normals():
     global selected_objects
+    object = bpy.context.scene.objects
 
     for obj in selected_objects:
-        bpy.context.view_layer.objects.active = bpy.context.scene.objects[obj.name]
-        bpy.ops.mesh.customdata_custom_splitnormals_add()
+        if obj.type == "MESH":
+            bpy.context.view_layer.objects.active = bpy.context.scene.objects[obj.name]
+            bpy.ops.mesh.customdata_custom_splitnormals_add()
+        else:
+            print(obj.name + " Not type mesh")
 
-def add_split_normals():
+def clear_split_normals():
     global selected_objects
 
     for obj in selected_objects:
         bpy.context.view_layer.objects.active = bpy.context.scene.objects[obj.name]
         bpy.ops.mesh.customdata_custom_splitnormals_clear()
 
-add_split_normals()
+def duplicate_objects():
+    to_unlink = []
+    global selected_objects
+    selected = bpy.data.objects
+
+    #adding a new collection to put all duplicate objects
+    duplicate_collection = bpy.data.collections.new(name="Duplicated")
+    bpy.context.scene.collection.children.link(duplicate_collection)
+
+    #duplicating selected objects
+    bpy.ops.object.duplicate()
+
+    #move all duplicated objects to created collection
+    for obj in selected_objects:
+        try:
+            duplicate_collection.objects.link(obj)
+            print("objects linked to new collection")
+        except RuntimeError:
+            pass
+        to_unlink.append(obj)
+    
+    for obj in to_unlink:
+        print("code here")
+
+duplicate_objects()
+
+#add_split_normals()
 
 def unregister():
     print("Disabling the addon")
