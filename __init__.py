59f21a3ecdd98c376de92c48db1a41bbefe06aca
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-04 08:45:27 -0400

    print(active_object.name)
    for obj in selected_objects:
        print("____________")
        #print(obj.name)
        print(active_object.name)
        active_object = bpy.data.objects[obj.name]
        #split_custom_normals
        print(active_object.name)
        print("____________")
        time.sleep(1)
    
    print(active_object.name)
    working

--- __init__.py
+++ __init__.py
@@ -11,20 +11,29 @@ bl_info = {
 def register():
     print("Enabled the addon")
 
+#clearing Blenders console
+os.system('cls')
+
+#Defining Variables
 scene = bpy.context.scene
 selected_objects = bpy.context.selected_objects
 split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add()
 active_object = bpy.context.view_layer.objects.active
 
+print(active_object.name)
+
+#Looping through selected objects and adding custom split normals
 for obj in selected_objects:
+    print("____________")
     #print(obj.name)
     print(active_object.name)
     active_object = bpy.data.objects[obj.name]
+    #split_custom_normals
     print(active_object.name)
-    #time.sleep(2)
-    #split_custom_normals
     print("____________")
-    #split_custom_normals
+    time.sleep(1)
+
+print(active_object.name)
 
 def unregister():
     print("Disabling the addon")
