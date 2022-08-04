9b67a51b9f6d086baec409623c4469c44afce413
QuentinOberon <quentin.steinke@oberontech.com>
2022-08-04 09:06:45 -0400

    updates

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Advanced Blender addon for exporting
 
-*   Duplicate objects
-*   Add custom split normals to all objects
-*   Combine objects based on parent
+***
+
+# What I want this addon to do:
+1. Duplicate all visible meshes
+2. Make all instances real
+3. Apply all modifiers and apply custom normals so the meshes look exactly like the do in the export as they do in Blender
+4. Rename UV maps to map1, map2 etc so UV maps the combined mesh doesn't add UV maps
+5. Combine all meshes by parent empty
+6. Export all combined meshes as separate fbx files with custom settings for Unreal Engine
+7. Delete all exported meshes
+
+***
--- __init__.py
+++ __init__.py
@@ -20,20 +20,27 @@ selected_objects = bpy.context.selected_objects
 split_custom_normals = bpy.ops.mesh.customdata_custom_splitnormals_add()
 active_object = bpy.context.view_layer.objects.active
 
-print(active_object.name)
-
 #Looping through selected objects and adding custom split normals
-for obj in selected_objects:
-    print("____________")
-    #print(obj.name)
-    print(active_object.name)
-    active_object = bpy.data.objects[obj.name]
-    #split_custom_normals
-    print(active_object.name)
-    print("____________")
-    time.sleep(1)
+def split_normals():
+    global active_object
+    global split_custom_normals
+    global selected_objects
 
-print(active_object.name)
+    print(active_object.name)
+
+    for obj in selected_objects:
+        print("____________")
+        print(obj.name)
+        print(active_object.name)
+        active_object = bpy.data.objects[obj.name]
+        #split_custom_normals
+        print(active_object.name)
+        print("____________")
+        time.sleep(1)
+
+    print(active_object.name)
+
+split_normals()
 
 def unregister():
     print("Disabling the addon")
