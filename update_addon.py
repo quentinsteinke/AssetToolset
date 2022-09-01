f08617c10026328591a0a4de75ba9fe9250ff0d7
Quentin Steinke <quentinsteinke@gmail.com>
2022-09-01 06:53:59 -0400

    update

--- update_addon.py
+++ update_addon.py
@@ -1,3 +1,4 @@
+from distutils.file_util import copy_file
 import os
 
 
@@ -10,17 +11,24 @@ file_name = [
 
 # xcopy args /r /q /c /e /h /i /k /s /x /y
 
-for file in file_name:
+user_path = os.path.expanduser("~")
 
-    current_dir = os.getcwd()
-    blender_addon_path = "C:\\Users\\Quentin Steinke\\AppData\\Roaming\\Blender Foundation\\Blender\\3.2\\scripts\\addons\\QuentinAddon"
 
-    if file.endswith(".py"):
-        os.system(f'copy "{current_dir}\\{file}" "{blender_addon_path}"')
-        pass
-    else:
-        current_dir = f'{current_dir}\\{file}'
-        blender_addon_path = f'{blender_addon_path}\\{file}'
-        files = os.listdir(current_dir)
-        for file in files:
+def copy_files():
+    for file in file_name:
+
+        current_dir = os.getcwd()
+        blender_addon_path = f"{user_path}\\AppData\\Roaming\\Blender Foundation\\Blender\\3.2\\scripts\\addons\\QuentinAddon"
+
+        if file.endswith(".py"):
             os.system(f'copy "{current_dir}\\{file}" "{blender_addon_path}"')
+            pass
+        else:
+            current_dir = f'{current_dir}\\{file}'
+            blender_addon_path = f'{blender_addon_path}\\{file}'
+            files = os.listdir(current_dir)
+            for file in files:
+                os.system(f'copy "{current_dir}\\{file}" "{blender_addon_path}"')
+
+
+#copy_file()
