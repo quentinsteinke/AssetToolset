f5e1193552ea111dce954b169ea6184b2aec3736
Quentin Steinke <quentinsteinke@gmail.com>
2022-09-02 07:31:02 -0400

    "update copy files"

--- testing.py
+++ testing.py
@@ -6,33 +6,34 @@ user_path = os.path.expanduser("~")
 destination_path = "\\AppData\\Roaming\\Blender Foundation\\Blender\\3.2\\scripts\\addons\\QuentinAddon"
 g_cwd = os.getcwd()
 
+print(f">{g_cwd}")
 
-def copy_all_Files(cwd):
+tab = "  "
+
+
+def copy_all_Files(cwd, c):
     search_files = []
     remove_files = []
-
+    dir_list = []
+    tab = c
     search_files = (os.listdir(cwd))
-
 
     # Copy python files
     for file in search_files:
-        print("------------")
-        print(f"> Checking {file}")
-        if file.endswith(".py"):
-            print(f"Coppying {file}")
-            print(f'copy "{cwd}\\{file}" "{user_path}\\{destination_path}"')
+        if file.endswith(".txt"):
+            print(f"{tab}  |<{file}")
             remove_files.append(file)
-            print("(Remaining files: ")
-            for i in search_files:
-                print(f"({i}")
-            print("------------")
+        elif os.path.isdir(f"{cwd}\\{file}") is True and file.startswith(".") == False:
+            tab += tab
+            print("")
+            print(f"{tab}>{file}")
+            print(f"{tab}  |")
+            new_cwd = f"{cwd}\\{file}"
+            copy_all_Files(new_cwd, tab)
+
+            dir_list.append(file)
         else:
-            os.listdir(f"{cwd}\\{file}")
-
-    # Remove copied files from search list
-    for file in remove_files:
-        print(f"Removing copied files {file}")
-        search_files.remove(file)
+            pass
 
 
-copy_all_Files(g_cwd)
+copy_all_Files(g_cwd, tab)
